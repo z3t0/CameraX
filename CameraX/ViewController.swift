45130a47b1991e2489637b45b4fa987ff27f541f
Rafi Khan <zetoslab@gmail.com>
2016-11-06 18:34:14 -0600

    initial commit

---
+++ CameraX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraX
+//
+//  Created by Rafi Khan on 2016-11-06.
+//  Copyright © 2016 Rafi Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
